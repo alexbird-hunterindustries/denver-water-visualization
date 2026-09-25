16b47ce2b1e45535569ac70ff54e41df2ed6d2f6
Alex Bird <alex.bird@hunter.global>
2026-09-25 05:45:34 -0700

    Use a line chart

--- docs/main.js
+++ docs/main.js
@@ -21,21 +21,12 @@ async function main() {
     const ctx = document.querySelector('main #chart canvas');
 
     new Chart(ctx, {
-        type: 'bar',
+        type: 'line',
         data: {
-            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
             datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
-                borderWidth: 1
+                label: 'Stuff',
+                data: [{x: '2016-12-25', y: 20}, {x: '2016-12-26', y: 10}]
             }]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true
-                }
-            }
         }
     });
 
