99392a1750a2bdbfbd71e0af19c2dd5572301d49
Alex Bird <alex.bird@hunter.global>
2026-09-25 10:01:04 -0700

    Extract method for fetching station data

--- docs/main.js
+++ docs/main.js
@@ -1,7 +1,6 @@
-async function chartOneStationPrecipitationFor2026(stationMap) {
-    const upperColoradoWatershed = [ 602, 505, 1014, 970, 335, 415 ];
+async function goGetTheRecentStationData(currentYear, stationId) {
     const params = {
-        "stationTriplets": "938:CO:SNTL",
+        "stationTriplets": `${stationId}:CO:SNTL`,
         "elements": "WTEQ",
         "duration": "MONTHLY",
         "beginDate": "2022-04-02",
@@ -12,16 +11,27 @@ async function chartOneStationPrecipitationFor2026(stationMap) {
     const apiResponse = await fetch('https://wcc.sc.egov.usda.gov/awdbRestApi/services/v1/data?' + queryParams).then(x => x.json())
     const items = apiResponse[0].data[0].values;
 
+    return Object.fromEntries(items
+        .filter(({year, month}) => year === currentYear || (year === currentYear - 1 && month > 10))
+        .map(({year, month, value}) => [`${year}-${month}`, value]));
+}
+
+async function chartOneStationPrecipitationFor2026(stationMap) {
+    const upperColoradoWatershed = [602, 505, 1014, 970, 335, 415];
+    const xAxis = [
+        { year: 2025, month: 11 },
+        { year: 2025, month: 12 },
+        ...Array.from({ length: 9 }).map((_, i) => ({ year: 2026, month: i + 1}))
+    ].map(({ year, month }) => `${year}-${month}`)
+    const currentYear = 2026
+    const stationData = await goGetTheRecentStationData(currentYear, 938);
     const chartContext = document.querySelector('main #chart canvas');
     let cumulativeSum = 0;
     const smoosh = thing => {
         cumulativeSum += thing;
         return cumulativeSum;
     }
-    const currentYear = 2026;
-    const chartData = items
-        .filter(({year, month}) => year === currentYear || (year === currentYear - 1 && month > 10))
-        .map(({year, month, value}) => ({x: `${year}-${month}`, y: smoosh(value)}))
+    const chartData = xAxis.map(yearMonth => ({x: yearMonth, y: smoosh(stationData[yearMonth]) }));
     new Chart(chartContext, {
         type: 'line',
         data: {
